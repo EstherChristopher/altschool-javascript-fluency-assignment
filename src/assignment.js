6db1e874e92364c07799803e4d63aa36e9397125
Esther Christopher <97364759+EstherChristopher@users.noreply.github.com>
2022-07-13 22:35:35 +0000

    I attempted challenges 1 and 2.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,19 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+ 
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+let sum = 0;
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  sum += arrayOfNumbers[i];
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+
+     return sum;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,12 +36,20 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+ 
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+let evenNumbers = 0
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  if (arrayOfNumbers[i] % 2 === 0) {
+      evenNumbers++;
+    }  
+ 
+  }
+    return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
-/**
+/**        
  * Challenge - 3
  * 
  * Given an array of numbers representing temperatures in Celsius,
